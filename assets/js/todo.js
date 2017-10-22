83f061c667158f755cd0f46a47a50f938cd63db0
Rudinei Silva <rudislpp@gmail.com>
2017-10-21 22:04:14 -0200

    project done

--- assets/js/todo.js
+++ assets/js/todo.js
@@ -1,19 +1,25 @@
 //check off specific todos by clicking
-$("li").click(function() {
+$("ul").on("click","li", function() {
   $(this).toggleClass("completed");
 });
 
 //click on x to delete todos
-$("span").click(function(e){
+$("ul").on("click", "span", function(e){
   $(this).parent().fadeOut(500, function(){
     $(this).remove();
   });
   // stop buble propagation
   e.stopPropagation();
-})
+});
 
 $("input[type='text']").keypress(function(event){
   if(event.which === 13){
-    $(this).val();
+    var todoText = $(this).val();
+    $(this).val("");
+    $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>");
   }
+});
+
+$(".material-icons").click(function(){
+  $("input[type='text']").fadeToggle();
 });
